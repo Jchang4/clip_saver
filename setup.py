75e576f4f808102cf12a4878391333dfce1e9926
Justin <justinchangmusic@gmail.com>
2023-11-18 10:12:43 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="clip_saver",
-    version="0.1",
+    version="0.2",
     packages=find_packages(),
     install_requires=["ultralytics", "supervision", "numpy", "pydantic"],
 )
