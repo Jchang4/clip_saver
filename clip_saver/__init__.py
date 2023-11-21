fc49bcbec8b033a24be73f20b3245beb36cc4139
Justin <justinchangmusic@gmail.com>
2023-11-21 08:21:15 -0500

    Add all objects to __init__ for easy access

--- clip_saver/__init__.py
+++ clip_saver/__init__.py
@@ -1,5 +1,10 @@
-from .base import Frame, StartAndEndFrames
-from .buffer import Buffer, SamplingBuffer
-from .callbacks import Callback, DetectionCallback
+from .base import Frame, MostAccurateFrame, StartAndEndFrames
+from .buffer import (
+    Buffer,
+    MostAccurateFrameBuffer,
+    SamplingBuffer,
+    StartAndEndFramesBuffer,
+)
+from .callbacks import Callback, DetectionCallback, run_in_background
 from .connection import Connection, get_rtsp_url
 from .detection import DetectionSaver
