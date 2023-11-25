36b1b73bfe06c3c459f29ccf084ef5dcb1603c78
Justin <justinchangmusic@gmail.com>
2023-11-25 18:07:12 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="clip_saver",
-    version="0.7.2",
+    version="0.8",
     packages=find_packages(),
     install_requires=[
         "lapx",
