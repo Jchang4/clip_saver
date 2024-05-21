1ae7170ee858e64fb59fdc8f535b4ad36b50f37e
Justin <justinchangmusic@gmail.com>
2024-05-21 12:31:38 -0400

    Bumnp version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="clip_saver",
-    version="0.8.4",
+    version="0.8.5",
     packages=find_packages(),
     install_requires=[
         "lapx",
