8ee1d84aabc436d79f158250f16fd36115d840c3
Justin <justinchangmusic@gmail.com>
2024-10-04 14:16:54 -0400

    Update version and packages

--- setup.py
+++ setup.py
@@ -2,12 +2,10 @@ from setuptools import find_packages, setup
 
 setup(
     name="clip_saver",
-    version="0.8.6",
+    version="0.9",
     packages=find_packages(),
     install_requires=[
         "lapx",
-        "numpy",
-        "pydantic",
         "supervision",
         "ultralytics",
     ],
