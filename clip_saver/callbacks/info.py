0b16ab2d8c49f3913a548676f37e1a4744ed5032
Justin <justinchangmusic@gmail.com>
2024-10-16 23:29:11 -0400

    PrintCallback: sometimes there is no track id and we get an error. Fix that by grabbing track id only if it exists

--- clip_saver/callbacks/info.py
+++ clip_saver/callbacks/info.py
@@ -14,9 +14,12 @@ class PrintInfoCallback(BaseCallback):
         detections = frame.detections
         print(f"Detections: {len(detections)} - {datetime.now().isoformat()}")
 
-        for track_id, class_id, conf in zip(
-            detections.tracker_id, detections.class_id, detections.confidence
+        for i, (class_id, conf) in enumerate(
+            zip(detections.class_id, detections.confidence)
         ):
+            track_id = (
+                detections.tracker_id[i] if detections.tracker_id is not None else -1
+            )
             print(f"Track ID: {track_id}, Class ID: {class_id}, Confidence: {conf}")
 
     def stop(self):
